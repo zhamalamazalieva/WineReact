2916d2ca6ff955a80e075838936afc8d2e112d68
zhamalamazalieva <jamalamazalieva@gmail.com>
2021-01-25 17:20:55 +0100

    fixed some code

--- wine-react/src/components/productItem/const.js
+++ wine-react/src/components/productItem/const.js
@@ -8,54 +8,61 @@ import img7 from '../../assets/img/png/product7.png';
 
 export const Item = [
     {
-        discount:"30",
-        img:{img1},
+        discount:'',
+        img:img1,
         name:"Silver Oak",
         oldPrice:"500.00",
-        newPrice:"450.00"
+        newPrice:"450.00",
+        sort:0
     },
     {
-        discount:"30",
-        img:{img2},
+        discount:30,
+        img:img2,
         name:"2015 Woodenhead",
         oldPrice:"358.00",
-        newPrice:"310.00"
+        newPrice:"310.00",
+        sort:0
     },
     {
         discount:20,
-        img:{img3},
+        img:img3,
         name:"2014 Red Hills",
         oldPrice:"500.00",
-        newPrice:"450.00"
+        newPrice:"450.00",
+        sort:1,
     },
     {
-        discount:"30",
-        img:{img4},
+        discount:30,
+        img:img4,
         name:"2012 Chardonnay",
         oldPrice:"550.00",
-        newPrice:"650.00"
+        newPrice:"650.00",
+        sort:2
     },
     {
-        discount:"30",
-        img:{img5},
+        discount:30,
+        img:img5,
         name:"Cabernet",
         oldPrice:"500.00",
-        newPrice:"450.00"
+        newPrice:"450.00",
+        sort:3
 
     },
     {
-        discount:"30",
-        img:{img6},
+        discount:30,
+        img:img6,
         name:"Mazzei sipi 2016",
         oldPrice:"500.00",
-        newPrice:"450.00"
+        newPrice:"450.00",
+        sort:3
     },
     {
-        discount:"30",
-        img:{img7},
+        discount:'',
+        img:img7,
         name:"Penfolds uncorks",
         oldPrice:"500.00",
-        newPrice:"450.00"
+        newPrice:"450.00",
+        sort:2
     },
   
 ]
